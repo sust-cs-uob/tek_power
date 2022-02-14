41ad9de3344b305f45a9b79a43c372e21912c913
Daniel Schien <dschien@gmail.com>
2022-02-14 14:56:54 +0000

    prep to redo failed jobs

--- tek_net.py
+++ tek_net.py
@@ -1,3 +1,4 @@
+import csv
 import datetime
 import logging
 import time
@@ -27,37 +28,53 @@ def send_rec_tek_command(conn, cmd_string):
 
 
 
-def run():
-    # print('test')
-    go = None
-    try:
-        go = connect(HOST, PORT)
+def run(args):
+    with open(args.csv, 'wb') as myfile:
+        wrtr = csv.writer(myfile, delimiter=',', quotechar='"')
 
-        send_rec_tek_command(go, ":SEL:CLR")
-        send_rec_tek_command(go, ":SEL:WAT")
-        send_rec_tek_command(go, ":DSE 2")
-        while True:
-            data_ready = False
-            while not data_ready:
-                resp = send_rec_tek_command(go, ":DSR?")
-                # print(resp)
-                if int(resp.strip()) == 2:
-                    # print('data ready')
-                    data_ready = True
-                time.sleep(.3)
-            # print('reading')
-            resp = send_rec_tek_command(go, ":FRD?")
-            now = datetime.datetime.now()
-            start_time = now.isoformat(sep=' ', timespec='milliseconds')
-            logger.info(f"{start_time},{float(resp.strip())}")
-            print(f"{start_time},{float(resp.strip())}")
-    except KeyboardInterrupt:
-        logger.info('closing connection')
+        # print('test')
+        go = None
+        try:
+            go = connect(HOST, PORT)
+
+            send_rec_tek_command(go, ":SEL:CLR")
+            send_rec_tek_command(go, ":SEL:WAT")
+            send_rec_tek_command(go, ":DSE 2")
+            while True:
+                data_ready = False
+                while not data_ready:
+                    resp = send_rec_tek_command(go, ":DSR?")
+                    # print(resp)
+                    if int(resp.strip()) == 2:
+                        # print('data ready')
+                        data_ready = True
+                    time.sleep(.3)
+                # print('reading')
+                resp = send_rec_tek_command(go, ":FRD?")
+                now = datetime.datetime.now()
+                start_time = now.isoformat(sep=' ', timespec='milliseconds')
+
+                wrtr.writerow([start_time, float(resp.strip())])
+                # logger.info(f"{start_time},{float(resp.strip())}")
+                print(f"{start_time},{float(resp.strip())}")
+        except KeyboardInterrupt:
+            logger.info('closing connection')
+            go.close()
+            sys.exit()
         go.close()
-        sys.exit()
-    go.close()
+
+def setup_parser(args):
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--csv', '-c', help="to write to")
+
+    args = parser.parse_args(args)
+    # print(args)
+    return args
+
 
 if __name__ == "__main__":
-    logger.info('starting logging')
-    run()
+    args = setup_parser(sys.argv[1:])
+    logger.info(f'starting logging to file {args.csv}')
+    run(args)
 
